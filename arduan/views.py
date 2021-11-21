68c536ab883977ed2baa941deeb7762da98f57ba
Vitaly <ivanov.arduan@yandex.ru>
2021-11-21 16:44:17 +0300

    Циклы в шаблонах.

--- arduan/views.py
+++ arduan/views.py
@@ -5,7 +5,17 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse('<h1>Hello World<h1>')
 
-
+my_family = 'Иванов Виталий Иванович'
+num = [1,2,3,4]
 def get_client_ip(request):
     ip = request.META.get('REMOTE_ADDR')
-    return render(request, 'user_info.html', {'ip_address': ip})
+    return render(request, 'user_info.html', {'ip_address': ip,
+                                              'my_family': my_family,
+                                              })
+
+
+def spisok(request, *args, **kwargs):
+    name_spisok = ['Иванов', 'Петров', 'Сидоров', 'Доброхвалов']
+
+    return render(request, 'user_info.html', {'name_spisok': name_spisok})
+
--- django_examp_url/urls.py
+++ django_examp_url/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from arduan.views import index, get_client_ip
+from arduan.views import index, get_client_ip, spisok
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
     # path('', index),
-    path('', get_client_ip)
+    path('', spisok),
+
+
 
 ]
