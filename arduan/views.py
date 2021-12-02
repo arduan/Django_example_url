235a7bd73965bd105414efdf27fd743217d85178
Vitaly <ivanov.arduan@yandex.ru>
2021-12-02 08:20:32 +0300

    Пробы с шаблонами с render.

--- arduan/views.py
+++ arduan/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 
 
 def index(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render({},request))
+    template = 'index.html'
+    return render(request, template)
 
 
